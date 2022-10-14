a659ebb5b13a559c9b95e3b0c642ca09739569d4
Saikiran8844 <nannapanenisaikiran8844@gmail.com>
2022-10-14 13:10:42 +0530

    my commits

---
+++ src/Components/login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import'./style.css';
+function Login(){
+    return(
+        <>
+        <div class="wrapper login">
+        <div class="container">
+            <div class="col-left">
+                <div class="login-text">
+                    <h2>Welcome!</h2>
+                    <p>Create your account.<br/>For Free!</p>
+                    <a href="#" class="btn">Sign Up</a>
+                </div>
+            </div>
+
+            <div class="col-right">
+                <div class="login-form">
+                    <h2>Login</h2>
+                    <form action="">
+                        <p>
+                            <label>Username/Email address<span>*</span></label>
+                            <input type="text" placeholder="Username or Email" required></input>
+                        </p>
+                        <p>
+                            <label>Password<span>*</span></label>
+                            <input type="password" placeholder="Password" required></input>
+                        </p>
+                        <p>
+                            <input type="submit" value="Sign In"></input>
+                        </p>
+                        <p>
+                            <a href="#">Forgot password?</a>
+                        </p>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+        </>
+    )
+}
+export default Login;
